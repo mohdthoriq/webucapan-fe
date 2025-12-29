9c256635bcc96fb00272685c6c5b331977f32b02
mhubaib <mhubaib500@gmail.com>
2025-12-29 08:40:59 +0700

    feat: implements auto numbering for invoice number

--- src/features/sales/invoices/invoice-form/hooks/use-invoice-form.ts
+++ src/features/sales/invoices/invoice-form/hooks/use-invoice-form.ts
@@ -1,7 +1,12 @@
 import { useEffect, useMemo } from 'react'
 import { useFieldArray, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { type FinanceNumber, type SalesInvoice, Status } from '@/types'
+import {
+  type FinanceNumber,
+  FinanceNumberType,
+  type SalesInvoice,
+  Status,
+} from '@/types'
 import {
   CreateInvoiceSchema,
   UpdateInvoiceSchema,
@@ -10,8 +15,10 @@ import {
 } from '../types/invoice-form.schema'
 import {
   useCreateInvoiceMutation,
+  useGenerateNextNumber,
   useUpdateInvoiceMutation,
 } from './use-invoice-form-mutation'
+import { useNavigate } from '@tanstack/react-router'
 
 type UseInvoiceFormProps = {
   currentRow?: SalesInvoice
@@ -23,6 +30,7 @@ export function useInvoiceForm({
   autoNumbering,
 }: UseInvoiceFormProps) {
   const isEdit = !!currentRow
+  const navigate = useNavigate()
 
   const defaultValues = useMemo(
     () =>
@@ -105,6 +113,7 @@ export function useInvoiceForm({
   })
 
   const createMutation = useCreateInvoiceMutation()
+  const generateNextNumber = useGenerateNextNumber()
   const updateMutation = useUpdateInvoiceMutation()
 
   const onSubmit = async (data: CreateInvoiceFormData) => {
@@ -113,11 +122,20 @@ export function useInvoiceForm({
         ...data,
         id: currentRow.id,
       } as UpdateInvoiceFormData
-      await updateMutation.mutateAsync(updateData)
+      const response = await updateMutation.mutateAsync(updateData)
       form.reset(data)
+      navigate({
+        to: `/sales/invoices/detail`,
+        state: { currentRowId: response.data.id } as Record<string, unknown>,
+      })
     } else {
-      await createMutation.mutateAsync(data)
+      const response = await createMutation.mutateAsync(data)
+      await generateNextNumber.mutateAsync(FinanceNumberType.sales_invoice)
       form.reset()
+      navigate({
+        to: `/sales/invoices/detail`,
+        state: { currentRowId: response.data.id } as Record<string, unknown>,
+      })
     }
   }
 
