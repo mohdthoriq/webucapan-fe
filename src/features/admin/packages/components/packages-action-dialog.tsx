d97a268d0bc56cb3cfd95eea018b2920d50510e5
mhubaib <mhubaib500@gmail.com>
2026-02-10 17:38:00 +0700

    feat: Create `PackagesActionDialog` component for adding and editing package details with dynamic feature inputs.

--- src/features/admin/packages/components/packages-action-dialog.tsx
+++ src/features/admin/packages/components/packages-action-dialog.tsx
@@ -169,7 +169,7 @@ export function PackagesActionDialog({
                     onClick={() => append('')}
                   >
                     <Plus className='h-4 w-4' />
-                    Tambah Fitur
+                    {fields.length === 0 ? 'Masukkan Fitur' : 'Tambah Fitur'}
                   </Button>
                 </div>
               </div>
