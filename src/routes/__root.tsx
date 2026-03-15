a9baa672e9fc40a4cd77c43837b586f202c2b0c4
mhubaib <mhubaib500@gmail.com>
2026-03-15 07:18:14 +0700

    feat(ui): add global floating whatsapp button - Create FloatingWhatsApp component with Tabler Icons - Add global WhatsApp button to the bottom-right corner in root layout - Comment out TanStack Query and Router Devtools temporarily

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
 import { type QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { useIsOffline } from '@/hooks/use-is-offline'
 import { Toaster } from '@/components/ui/sonner'
 import { NavigationProgress } from '@/components/navigation-progress'
 import { GeneralError } from '@/features/errors/general-error'
 import { NotFoundError } from '@/features/errors/not-found-error'
 import { OfflineError } from '@/features/errors/offline-error'
+import { FloatingWhatsApp } from '@/components/floating-whatsapp'
 
 function RootComponent() {
   const isOffline = useIsOffline()
@@ -21,10 +22,11 @@ function RootComponent() {
       <NavigationProgress />
       <Outlet />
       <Toaster duration={5000} position='top-center' richColors />
+      <FloatingWhatsApp />
       {import.meta.env.MODE === 'development' && (
         <>
-          <ReactQueryDevtools buttonPosition='bottom-right' />
-          <TanStackRouterDevtools position='bottom-right' />
+          {/* <ReactQueryDevtools buttonPosition='bottom-right' /> */}
+          {/* <TanStackRouterDevtools position='bottom-right' /> */}
         </>
       )}
     </>
